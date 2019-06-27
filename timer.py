4dec0c88bb72ae996ad6491bf0e1c8ce62b87f1a
rtybanana <rorypinkney@yahoo.co.uk>
2019-06-27 13:49:51 +0100

    - Added toast notification to warn a user that they are approaching two       minutes of inactivity, after which the timer will auto pause. - Added some comments and constants to make maintenance easier - Renamed the class from 'App' to 'Timer' - Fixed a small bug where user won't be reminded to restart timer if       they manually paused it

--- timer.py
+++ timer.py
@@ -4,6 +4,11 @@ from win10toast import ToastNotifier
 import datetime
 
 
+TIMEOUT_TIME = 120
+NOTIFY_TIME = TIMEOUT_TIME - 10
+IDLE_TIME = 300
+
+
 def get_idle_duration():
     lastInputInfo = LASTINPUTINFO()
     lastInputInfo.cbSize = sizeof(lastInputInfo)
@@ -18,11 +23,8 @@ class LASTINPUTINFO(Structure):
         ('dwTime', c_uint),
     ]
 
-NOTIFY_TIME = 110
-TIMEOUT_TIME = 120
 
-
-class App:
+class Timer:
     def __init__(self):
         self.root = Tk()
         self.root.title("Work Timer")
@@ -34,6 +36,7 @@ class App:
         self.pause_timestamp = 0
         self.paused_time = datetime.timedelta(0)
         self.paused = False
+        self.warning = False
         self.idle = False
 
         self.lbl_timer = Label(text="", font=("Courier", 40))
@@ -45,16 +48,6 @@ class App:
 
         self.root.mainloop()
 
-    def pause(self):
-        self.paused = not self.paused
-        if self.paused is False:
-            self.paused_time += datetime.datetime.now() - self.pause_timestamp
-            self.update_clock()
-            self.btn_pause.config(text="Pause")
-        else:
-            self.pause_timestamp = datetime.datetime.now()
-            self.btn_pause.config(text="Start")
-
     def update_clock(self):
         now = datetime.datetime.now() - self.start_time - self.paused_time
         hours, remainder = divmod(now.seconds, 3600)
@@ -62,41 +55,56 @@ class App:
         text = '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
         self.lbl_timer.configure(text=text)
 
-        if not self.paused and get_idle_duration() < TIMEOUT_TIME:
-            self.root.after(1, self.update_clock)
-        elif get_idle_duration() >= NOTIFY_TIME:
-            self.clock_timeout()
+        # clock operating as normal
+        if not self.paused and get_idle_duration() < NOTIFY_TIME:
+            if self.warning:                            # reset warning toast flag if the user cancelled the auto pause
+                self.warning = False
+            self.root.after(1, self.update_clock)       # call self after one millisecond
 
+        # warning toast that the timer is going to auto pause soon, user can activate an input to cancel auto pause
+        elif NOTIFY_TIME <= get_idle_duration() < TIMEOUT_TIME:
+            if not self.warning:
+                toaster = ToastNotifier()
+                toaster.show_toast("Are you still there?",
+                                   "Wiggle your mouse to continue timing",
+                                   icon_path=r'WorkTimer.ico',
+                                   duration=5,
+                                   threaded=True)
+                self.warning = True
+            self.root.after(1, self.update_clock)
+
+        # timer auto pause and update_idle() called
         elif get_idle_duration() >= TIMEOUT_TIME:
             self.idle = True
             self.pause()
             self.update_idle()
 
-    def clock_timeout(self):
-        notified = False
-        if not notified:
-            toaster = ToastNotifier()
-            toaster.show_toast("Are you still there?",
-                               "Wiggle your mouse to continue timing",
-                               icon_path=r'WorkTimer.ico',
-                               duration=10,
-                               threaded=True)
-            notified = True
-        if get_idle_duration() > 120:
-            self.update_idle()
-
+    # function to monitor mouse and keyboard input to remind the user to restart the timer after a period of inactivity
     def update_idle(self):
-        toaster = ToastNotifier()
-        if get_idle_duration() < 120 and self.idle:
+        if get_idle_duration() < TIMEOUT_TIME and self.idle:
+            toaster = ToastNotifier()
             toaster.show_toast("Welcome back",
-                               "Remember to unpause the work timer if you're restarting work",
+                               "Remember to unpause the timer if you're restarting work",
                                icon_path=r'WorkTimer.ico',
+                               duration=5,
                                threaded=True)
             self.idle = False
-        elif get_idle_duration() > 300 and not self.idle:
+        elif get_idle_duration() > IDLE_TIME and not self.idle:
             self.idle = True
 
-        self.root.after(1, self.update_idle)
+        self.root.after(1, self.update_idle)            # call self after one millisecond
+
+    # pause function for hard coded function calls and also the pause button callback
+    def pause(self):
+        self.paused = not self.paused
+        if self.paused is False:
+            self.paused_time += datetime.datetime.now() - self.pause_timestamp
+            self.btn_pause.config(text="Pause")
+            self.update_clock()
+        else:
+            self.pause_timestamp = datetime.datetime.now()
+            self.btn_pause.config(text="Start")
+            self.update_idle()
 
 
-app = App()
+app = Timer()
